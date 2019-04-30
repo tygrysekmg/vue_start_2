a6dced6d6d73041244c4f8abd7eaaf2c22bfc96d
Grzegorz <w.luk1972@wp.pl>
2019-04-30 19:30:02 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div id="app"> 
+  <HelloWorld msg="Zaczynamy pisać w Vue.js!"/>  
+  <HelloAgain /> 
+  </div>
+  </template>
+  <script>
+  import HelloWorld from './components/HelloWorld.vue';
+  import HelloAgain from './components/HelloAgain.vue'
+  export default 
+  { 
+  name: 'app',  components: {    HelloWorld, HelloAgain  
+  }}
+  </script>
+  <style>
+  #app {
+  font-family:
+  sans-serif;
+  text-align: center;
+  color: #2c3e50; 
+  margin-top: 60px;}
+  </style>
---
+++ src/components/HelloAgain.vue
@@ -0,0 +1,29 @@
+<template>
+ <p class="hello-again"> This is Grzegorz master vue.js  component.
+ <button v-on:click="incrementUp">Add One</button>
+ <br> 
+ <span>Current value of the counter: {{ counter }}</span> </p>
+ </template>
+
+
+<script>
+export default
+ { name: 'HelloAgain', data() {  
+ return {         counter: 0     } },
+ methods: {     incrementUp: function() 
+ {         this.counter++     }
+ }
+ }
+ </script>
+
+
+<style scoped>
+p {
+
+ font-size: 16px; text-align: center;
+ color: tomato;}
+ 
+ button {  
+ display: block;    margin: 10px auto;    background: tomato;    color: white;    border: none;    padding: 5px 10px;    font-size: 16px;    border-radius: 4px;    cursor: pointer;
+ }
+ </style>
